1f885aa199972dc51ad1b981644ed460ddc4e8c3
Andrey Khranovsky <akhranovskiy@gmail.com>
2020-05-26 20:00:47 +0200

    React app basic

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,24 @@ group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib-js"))
+
+    implementation("org.jetbrains:kotlin-react:16.13.0-pre.94-kotlin-1.3.70")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.0-pre.94-kotlin-1.3.70")
+    implementation(npm("react", "16.13.1"))
+    implementation(npm("react-dom", "16.13.1"))
+
+    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.94-kotlin-1.3.70")
+    implementation(npm("styled-components"))
+    implementation(npm("inline-style-prefixer"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.5")
 }
 
 kotlin.target.browser { }
