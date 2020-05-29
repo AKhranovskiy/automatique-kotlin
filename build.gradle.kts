b60104f3b6f59d662bfc1ceade26bbab16f1df7e
Andrey Khranovsky <akhranovskiy@gmail.com>
2020-05-29 19:13:25 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.3.72"
+    kotlin("js") version "1.3.72"
 }
 
 group = "org.example"
@@ -13,17 +13,17 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-js"))
+    testImplementation(kotlin("test-js"))
 
-    implementation("org.jetbrains:kotlin-react:16.13.0-pre.94-kotlin-1.3.70")
-    implementation("org.jetbrains:kotlin-react-dom:16.13.0-pre.94-kotlin-1.3.70")
+    implementation("org.jetbrains:kotlin-react:16.13.1-pre.105-kotlin-1.3.72")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.105-kotlin-1.3.72")
     implementation(npm("react", "16.13.1"))
     implementation(npm("react-dom", "16.13.1"))
 
-    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.94-kotlin-1.3.70")
+    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.105-kotlin-1.3.72")
+
     implementation(npm("styled-components"))
     implementation(npm("inline-style-prefixer"))
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.5")
 }
 
 kotlin.target.browser { }
